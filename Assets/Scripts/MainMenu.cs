f05adf431a8f70816e6c31585a68baa0424171be
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2016-02-15 00:45:01 -0600

    Menu principal terminado, con delizamiento de los fondos y los botones atras en el menú opciones y selección de nivel, botón de ayuda terminado con boton que cierra la ventana

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,66 +4,66 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-	public Canvas selectLevel; //menu de seleccion de niveles
-	public Button startText;//boton start
-	public Button optionText;//boton de opciones
-	public Button backText;// boton para regresar al menu principal
-	public GameObject background;//pensado para mover la imagen al presionar un boton 
-	public float speed;//velocidad que seria utilizada para mover el fondo
-
-
-	/* Use this for initialization
+    /* Use this for initialization
 	 * TODO el codigo va aqui
 	 */
+    public Canvas helpMenu;
+    public Button startText;
+    public Button optionText;
+    public Button helpButton;
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
 
 	void Start () {
-		selectLevel = selectLevel.GetComponent<Canvas> ();
-		startText = startText.GetComponent<Button> ();
-		selectLevel.enabled = false;
-		backText.enabled = false;
-		backText.gameObject.SetActive (false);//metodo que oculta el boton back
-	}
+        cameraTransform = Camera.main.transform;
+        helpMenu = helpMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        optionText = optionText.GetComponent<Button>();
+        helpButton = helpButton.GetComponent<Button>();
+        helpMenu.enabled = false;
+    }
+
+    void Update(){
+        if(cameraDesiredLookAt != null) {
+            cameraTransform.position = Vector3.Lerp(
+                new Vector3 (cameraTransform.position.x, cameraTransform.position.y, -2),
+                cameraDesiredLookAt.position,
+                3 * Time.deltaTime);
+        }
+    }
+    /*
+     * Metodo que muestra el menu de ayuda del menu principal
+     */
+    public void helpPress(){
+        helpMenu.enabled = true;
+        startText.enabled = false;
+        optionText.enabled = false;
+        helpButton.enabled = false;
+    }
+    /*
+     *Metodo que cierra la ventana emergente del menu de ayuda
+     */
+    public void closePress(){
+        helpMenu.enabled = false;
+        startText.enabled = true;
+        optionText.enabled = true;
+        helpButton.enabled = true;
+    }
+
 	/* Metodo utilizado al presionar el boton start que mandara a la seleccion de nivel
 	 * se encarga de activar los niveles y desactivar los demas botones de la ventana principal
 	 * 
 	 */
-	public void startPress(){
-		backgroundMover ();
-		selectLevel.enabled = true;
-		startText.enabled = false;
-		startText.gameObject.SetActive (false);
-		optionText.enabled = false;
-		optionText.gameObject.SetActive (false);
-		backText.enabled = true;
-		backText.gameObject.SetActive (true);
-	}
-	/* Metodo utilizado al presionar el boton atras en el menu de seleccion de niveles o de
-	 * opciones, que regresa a la pantana principal del menu del juego
-	 * 
-	 */
-	public void backPress(){
-		selectLevel.enabled = false;
-		startText.enabled = true;
-		startText.gameObject.SetActive (true);
-		optionText.enabled = true;
-		optionText.gameObject.SetActive (true);
-		backText.enabled = false;
-		backText.gameObject.SetActive (false);
-	}
-	/* 
-	 * 
-	 */
-	public void optionPress(){
-		
-	}
-
-	public void backgroundMover(){
-		Debug.Log ("Se entro al metodo backgroundMover");
-		background.transform.position = Vector3.left * speed;
-		Debug.Log ("Se leyo el codigo del background transform");
-	}
+	public void lookAtMenu(Transform targetPosition){
+        cameraDesiredLookAt = targetPosition;
+        //Camera.main.transform.position = new Vector3 (targetPosition.position.x,targetPosition.position.y, -2);
+    }
+    
 	//El metodo sera movido a el script de la seleccion de los niveles a no ser que se haga todo el script del menu en uno solo
 	public void startLevel(){
-		Application.LoadLevel (1);
+        UnityEngine.SceneManagement.LoadSceneMode mode;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1, mode = UnityEngine.SceneManagement.LoadSceneMode.Single);
+		//Application.LoadLevel (1); obsolete
+        
 	}
 }
