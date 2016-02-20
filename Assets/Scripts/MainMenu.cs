8acd8dcd7ebffda401fec11befa34d646caaf956
Roberto Torres Jiménez <iruga_cross@hotmail.com>
2016-02-20 17:12:53 -0600

    creación del seleccionador de armas

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,19 +8,46 @@ public class MainMenu : MonoBehaviour {
 	 * TODO el codigo va aqui
 	 */
     public Canvas helpMenu;
+    public Canvas selectWeaponMenu;
     public Button startText;
     public Button optionText;
     public Button helpButton;
     private Transform cameraTransform;
     private Transform cameraDesiredLookAt;
-
-	void Start () {
+    //canvas select level 
+    public Button level1;
+    public Button level2;
+    public Button level3;
+    public Button level4;
+    public Button level5;
+    public Button level6;
+    public Button level7;
+    public Button level8;
+    public Button level9;
+    public Button level10;
+    public Button back;
+   
+    void Start () {
         cameraTransform = Camera.main.transform;
         helpMenu = helpMenu.GetComponent<Canvas>();
+        selectWeaponMenu = selectWeaponMenu.GetComponent<Canvas>();
         startText = startText.GetComponent<Button>();
         optionText = optionText.GetComponent<Button>();
         helpButton = helpButton.GetComponent<Button>();
         helpMenu.enabled = false;
+        selectWeaponMenu.enabled = false;
+        //canvas select level 
+        level1 = level1.GetComponent<Button>();
+        level2 = level2.GetComponent<Button>();
+        level3 = level3.GetComponent<Button>();
+        level4 = level4.GetComponent<Button>();
+        level5 = level5.GetComponent<Button>();
+        level6 = level6.GetComponent<Button>();
+        level7 = level7.GetComponent<Button>();
+        level8 = level8.GetComponent<Button>();
+        level9 = level9.GetComponent<Button>();
+        level10 = level10.GetComponent<Button>();
+        back = back.GetComponent<Button>();
     }
 
     void Update(){
@@ -43,11 +70,44 @@ public class MainMenu : MonoBehaviour {
     /*
      *Metodo que cierra la ventana emergente del menu de ayuda
      */
-    public void closePress(){
+    public void helpclosePress(){
         helpMenu.enabled = false;
         startText.enabled = true;
         optionText.enabled = true;
         helpButton.enabled = true;
+    }
+
+    public void selectWeaponPress()
+    {
+        selectWeaponMenu.enabled = true;
+        level1.enabled = false;
+        level2.enabled = false;
+        level3.enabled = false;
+        level4.enabled = false;
+        level5.enabled = false;
+        level6.enabled = false;
+        level7.enabled = false;
+        level8.enabled = false;
+        level9.enabled = false;
+        level10.enabled = false;
+        back.enabled = false;
+    }
+
+    public void selectWeaponclose()
+    {
+
+        selectWeaponMenu.enabled = false;
+        level1.enabled = true;
+        level2.enabled = true;
+        level3.enabled = true;
+        level4.enabled = true;
+        level5.enabled = true;
+        level6.enabled = true;
+        level7.enabled = true;
+        level8.enabled = true;
+        level9.enabled = true;
+        level10.enabled = true;
+        back.enabled = true;
     }
 
 	/* Metodo utilizado al presionar el boton start que mandara a la seleccion de nivel
